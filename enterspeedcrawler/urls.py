6995ac750350be5dda8351a110e2fd945c024e67
Alin Baltatescu <alinbaltatescu@Alins-Mac-mini.local>
2020-10-29 17:49:27 +0100

    added index file

--- enterspeedcrawler/urls.py
+++ enterspeedcrawler/urls.py
@@ -19,5 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('/', admin.site.urls),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
